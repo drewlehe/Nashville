d34db1b09c45c0e0fd55212be311106ee195834e
drewlehe <alehe90@gmail.com>
2019-08-04 16:43:26 -0500

    divided scraper.py between sales and parcels

---
+++ parcelScraper.py
@@ -0,0 +1,33 @@
+﻿import pandas as pd
+from bs4 import BeautifulSoup as bs
+import urllib3
+import requests
+import json
+
+cardframe = pd.DataFrame()
+for col in nucols:
+    cardframe[col]= col
+#If I do this as a list comprehension, cardframe[col] = [col for col in nucols], I get a transposed list for some reason.
+print(type(subsoup))
+maindf=pd.DataFrame()
+mainlst= []
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+for lot in range(152802,155001):
+    address = "http://www.padctn.org/prc/property/{}/print".format(lot)
+    nulist=[]
+    card = requests.get(address, headers=headers)
+    cardsoup = bs(card.text, 'html.parser')
+    results = cardsoup.find_all('ul', class_='att')
+    subsoup = [lst.text for result in results for lst in result.find_all('li')]
+    keys=[]
+    values=[]
+    for string in subsoup:
+        if ':' not in string:
+            continue
+        else:
+            values.append(string.split(':')[1].strip())
+            keys.append(string.split(':')[0].strip())
+    websters= dict(zip(keys,values))
+    with open('Parcels/parcel{}'.format(lot), 'w') as file:
+        json.dump(websters, file)
+    mainlst.append(websters)
