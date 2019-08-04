6883bfb4721cc93d8f40d11edf186a9e6c9f3102
drewlehe <alehe90@gmail.com>
2019-08-04 16:49:07 -0500

    My script to combine all sales dataframes, could be in scraper.py

---
+++ salescreator.py
@@ -0,0 +1,16 @@
+﻿#script to combine all Sales datasets into one csv.
+
+import pandas as pd
+
+df2015 = pd.read_csv('sales2015.csv')
+df2016 = pd.read_csv('sales2016.csv')
+df2017 = pd.read_csv('sales2017.csv')
+df2018 = pd.read_csv('sales2018.csv')
+df2019 = pd.read_csv('sales2019.csv')
+
+dfmain = df2015.append(df2016)
+dfmain= dfmain.append(df2017)
+dfmain = dfmain.append(df2018)
+dfmain=dfmain.append(df2019)
+
+dfmain.to_csv('sales.csv', index=False)
