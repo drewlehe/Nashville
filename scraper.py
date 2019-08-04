b3b81dfd60dcde068bb8a9f61e9fdb413b33aba1
Drew <alehe90@gmail.com>
2019-08-04 18:31:10 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -57,3 +57,16 @@ for f in glob.glob("2015*.xls"):
     df = pd.read_excel(f)
     df2015 = df2015.append(df,ignore_index=True)
     df2015.to_csv(r'sales2015.csv', header=True)
+
+df2015 = pd.read_csv('sales2015.csv')
+df2016 = pd.read_csv('sales2016.csv')
+df2017 = pd.read_csv('sales2017.csv')
+df2018 = pd.read_csv('sales2018.csv')
+df2019 = pd.read_csv('sales2019.csv')
+
+dfmain = df2015.append(df2016)
+dfmain= dfmain.append(df2017)
+dfmain = dfmain.append(df2018)
+dfmain=dfmain.append(df2019)
+
+dfmain.to_csv('sales.csv', index=False)
