ede5d8211b664083cfef57dac3daca984f95e5c6
drewlehe <alehe90@gmail.com>
2019-08-06 20:59:29 -0500

    Cleaned up script

--- scraper.py
+++ scraper.py
@@ -3,10 +3,13 @@ from bs4 import BeautifulSoup as bs
 import urllib3
 import requests
 import json
-page = requests.get('http://www.padctn.org/services/recent-sales/')
-soup = bs(page.text, 'html.parser')
 from urllib.request import urlopen, urlretrieve, quote
 from openpyxl import Workbook
+
+if __name__ == '__main__':
+
+page = requests.get('http://www.padctn.org/services/recent-sales/')
+soup = bs(page.text, 'html.parser')
 u = urlopen(site)
 try:
     html = u.read().decode('utf-8')
@@ -14,11 +17,9 @@ finally:
     u.close()
 
 with open('davidson.csv', 'w', newline='') as csvfile:
-
     propertyWriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
     for link in soup.select('a[href^="http://"]'):
         href = link.get('href')
-
         # Make sure it has one of the correct extensions
         if not any(href.endswith(x) for x in ['.xls','.xlsx']):
             continue
@@ -32,28 +33,8 @@ with open('davidson.csv', 'w', newline='') as csvfile:
 
 #Creating a DataFrame and Excel file for each year's data.
 import glob
-df2019=pd.DataFrame()
-df2018=pd.DataFrame()
-df2017=pd.DataFrame()
-df2016=pd.DataFrame()
-df2015=pd.DataFrame()
-for f in glob.glob("2019*.xls"):
+dfsales=pd.DataFrame()
+for f in glob.glob("201*.xls"):
     df = pd.read_excel(f)
-    df2019 = df2019.append(df,ignore_index=True)
-    df2019.to_csv(r'sales2019.csv', header=True)
-for f in glob.glob("2018*.xls"):
-    df = pd.read_excel(f)
-    df2018 = df2018.append(df,ignore_index=True)
-    df2018.to_csv(r'sales2018.csv', header=True)
-for f in glob.glob("2017*.xls"):
-    df = pd.read_excel(f)
-    df2017 = df2017.append(df,ignore_index=True)
-    df2017.to_csv(r'sales2017.csv', header=True)
-for f in glob.glob("2016*.xls"):
-    df = pd.read_excel(f)
-    df2016 = df2016.append(df,ignore_index=True)
-    df2016.to_csv(r'sales2016.csv', header=True)
-for f in glob.glob("2015*.xls"):
-    df = pd.read_excel(f)
-    df2015 = df2015.append(df,ignore_index=True)
-    df2015.to_csv(r'sales2015.csv', header=True)
+    dfsales = dfsales.append(df,ignore_index=True)
+    dfsales.to_csv(r'sales.csv', header=True)
