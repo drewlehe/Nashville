c626078764336da87a270c9a46dbc94005657b93
drewlehe <alehe90@gmail.com>
2019-08-11 09:59:36 -0500

    message

---
+++ jsonmerger.py
@@ -0,0 +1,24 @@
+"""Script for joining scraped web data with sales data into a dataframe"""
+import json
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+
+    result = []
+    for lot in range(1, 270000):
+        infile = "/Users/alehe/Desktop/Nashville/Parcels/parcel{}".format(
+            lot)
+        with open(infile, 'r') as infile:
+            try:
+                result.append(json.load(infile))
+            except ValueError:
+                continue
+
+    df = pd.DataFrame(result)
+
+    sales = pd.read_csv('/Users/alehe/OneDrive/Documents/Jupyter/SharpestMinds/sales.csv')
+
+    jdf = df.merge(sales, right_on='Parcel ID', left_on='Map & Parcel', how='left')
+
+    jdf.to_csv('mergednew.csv')
