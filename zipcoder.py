687715fbece16d48ff83458e47ba50ba6d4b9d51
drewlehe <alehe90@gmail.com>
2019-08-16 17:00:31 -0500

    hid my API key

---
+++ zipcoder.py
@@ -0,0 +1,25 @@
+import googlemaps
+import pandas as pd
+
+with open(“c:/Users/alehe/Documents/google api key.txt”) as file:  
+    API_KEY = file.read() 
+
+ADDS= df['Location']
+
+if __name__ == '__main__':
+    gmaps=googlemaps.Client(key=API_KEY)
+    zips = []
+    for parcel in ADDS:
+        placeresult= gmaps.find_place(input= '{} Nashville, TN'.format(parcel), input_type='textquery',fields=['formatted_address'])
+        zipcode=str(placeresult['candidates'])[-13:-8]
+        zips.append(zipcode)
+    
+    def zipper(old_df):
+        df=old_df.copy()
+        df.insert(1,'Zipcode', '')
+        i=0
+        for parcel in df['Location']:
+            df['Zipcode'].loc[i] = zips[i]
+            i+=1
+        return df
+    
