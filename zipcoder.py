1e55c397938ee596e27f04f8b4158dea5e5ad5b3
drewlehe <alehe90@gmail.com>
2019-08-16 18:02:12 -0500

    clean/debugged zipcoder

--- zipcoder.py
+++ zipcoder.py
@@ -1,25 +1,25 @@
 import googlemaps
 import pandas as pd
 
-with open(“c:/Users/alehe/Documents/google api key.txt”) as file:  
-    API_KEY = file.read() 
+with open(r'C:\Users\alehe\OneDrive\Documents\google api key.txt',encoding='utf-8') as file:
+    API_KEY = file.read()
 
-ADDS= df['Location']
 
 if __name__ == '__main__':
-    gmaps=googlemaps.Client(key=API_KEY)
+    df = old_df.copy()
+    ADDS = df['Location']
+    gmaps = googlemaps.Client(key=API_KEY)
     zips = []
     for parcel in ADDS:
-        placeresult= gmaps.find_place(input= '{} Nashville, TN'.format(parcel), input_type='textquery',fields=['formatted_address'])
-        zipcode=str(placeresult['candidates'])[-13:-8]
+        placeresult = gmaps.find_place(input='{} Nashville, TN'.format(
+            parcel), input_type='textquery', fields=['formatted_address'])
+        zipcode = str(placeresult['candidates'])[-13:-8]
         zips.append(zipcode)
-    
+
     def zipper(old_df):
-        df=old_df.copy()
-        df.insert(1,'Zipcode', '')
-        i=0
+        df.insert(1, 'Zipcode', '')
+        i = 0
         for parcel in df['Location']:
             df['Zipcode'].loc[i] = zips[i]
-            i+=1
+            i += 1
         return df
-    
