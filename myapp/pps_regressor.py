7d4a6c0f11fc931a8e5103f27a27292d574ad644
Drew <alehe90@gmail.com>
2019-09-19 19:34:27 -0500

    Working model that displays price

---
+++ myapp/pps_regressor.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd 
+import lightgbm as lgb
+import json
+
+DENSITIES = {0:"SINGLE FAM", 1: "PLEX", 2: "CONDO", 3: "HRISE CONDO"}
+SEGMENTS = {0: 'C', 1: 'B', 2: 'A', 3:'X'}
+
+def prediction(input_json):
+    model = lgb.Booster(model_file='pps-model.txt')
+    with open('selection.json') as testfile:
+        bokeh_dict = json.load(testfile)
+    lst = [dict(bokeh_dict)]
+    tester = pd.DataFrame(lst)
+    tester['Log-Built'] = np.log(tester['Year Built'])
+    tester['Log-Fixtures'] = np.log(int(tester['Fixtures']))
+    tester['Log-SqFt'] = np.log(int(tester['Square Footage']))
+    tester['Neighborhood'] = tester['Neighborhood'].astype(float)
+    with open('meanpps.json', 'r') as nbhdpps:
+        nbhd_dict = json.load(nbhdpps)
+    meanpps = pd.DataFrame(nbhd_dict, index=None)
+    meanpps.index=meanpps.index.astype(float)
+    nbhds = list(meanpps.index)
+    tester=pd.merge(how = 'left', left = tester, right = meanpps,left_on="Neighborhood", right_index=True)
+    tester['Log-NbhdPPS'] = np.log(tester['Neighborhood'])
+    tester['Neighborhood'] = str(tester['Neighborhood'])
+    tester['Building-Grade'] = tester['Building-Grade'].map(lambda x: SEGMENTS[x])
+    tester['Building-Type-Custom'] = tester['Building-Type-Custom'].map(lambda x: DENSITIES[x])
+    X = pd.get_dummies(tester[['Year', 'Log-Built', 'Neighborhood', 'Log-Fixtures', 
+                               'Building-Type-Custom', 'Quarter', 'Building-Grade', 
+                               'Log-SqFt', 'Log-NbhdPPS']])
+    price_pred = model.predict(X)
+    return price_pred
