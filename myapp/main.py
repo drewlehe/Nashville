f654084b1650b465d2a7e047fa39168ac4a0c0aa
Drew <alehe90@gmail.com>
2019-09-22 20:00:32 -0500

    took out extraneous app buttons

--- myapp/main.py
+++ myapp/main.py
@@ -1,43 +1,46 @@
 import bokeh
-from bokeh.layouts import column, widgetbox
-from bokeh.models.widgets import Button, Paragraph, Dropdown, Slider, TextInput, CheckboxButtonGroup, Select, RadioButtonGroup, RadioGroup
+from bokeh.layouts import widgetbox
+from bokeh.models.widgets import *
 from bokeh.plotting import curdoc, show, figure
 from bokeh.embed import file_html, components
 import json
 import pps_regressor
+import pandas as pd
+
+with open('meanpps.json', 'r') as nbhdpps:
+        nbhd_dict = json.load(nbhdpps)
+meanpps = pd.DataFrame(nbhd_dict, index=None)
+hoods = list(meanpps.index)
 
 def update_data(event):
     bldg_data = {
-        'Neighborhood': str(float(nbhd.value_input)),
+        'Neighborhood': str(float(nbhd.value)),
         'Building-Type-Custom': density.active,
-        'Fixtures': fixtures.value,
         'Year Built': int(yearbuilt.value_input),
         'Building-Grade': segment.active,
         'Square Footage': squarefootage.value,
         'Renovation': reno_new.active,
         'Quarter': quarter.active,
         'Year': 2019}
-    
+    if reno_new.active == 1:
+        bldg_data['Year Built'] = 2019
+    else:
+        bldg_data['Year Built'] = int(yearbuilt.value_input)
     price_pred = pps_regressor.prediction(bldg_data)
     p.text = '$' + str(price_pred[0] * bldg_data['Square Footage'])
 
 # Create some widgets
-nbhd = TextInput(placeholder="4-digit #", title="Neighborhood Code:")
-densities = {"Single-Family":"SINGLE FAM", "Plex": "PLEX", "Mid-Rise": "CONDO", "High-Rise": "HRISE CONDO"}
-density = RadioButtonGroup(labels=['Single-Family', 'Plex', 'Mid-Rise', 'High-Rise'])
+nbhd = Select(title="Neighborhood:", options=list(hoods))
+density = RadioButtonGroup(labels=['Single-Family', 'Multi-Family'])
 reno_new = RadioButtonGroup(labels=["Renovation", "New Build"])
 #If renovation, select year built of the property:
 yearbuilt = TextInput(placeholder="4-digit #", title="Year of the original structure (if renovation):")
 squarefootage = Slider(start=500, end=3500, value=500, step=10, title="Square Footage")
-segments = {"Affordable": "C", "Mid-Range":"B", "High-End":"A", "Ultra-Lux":"X"}
-segment = RadioButtonGroup(
-        labels=["Affordable", "Mid-Range", "High-End", "Luxury"])
-quarters = [("One", 1), ("Two", 2), ("Three", 3), ("Four", 4)]
+segment = RadioButtonGroup(labels=["Affordable", "Mid-Range", "High-End"])
 quarter = RadioGroup(labels = ['First', 'Second', 'Third', 'Fourth'])
-fixtures = Slider(start=3, end=20, value = 4, step=1, title="Water Fixtures")
+fixtures = Slider(start=3, end=20, value=3, step=1, title="Water Fixtures")
 calculate = Button(label="Get Price", button_type="success")
 calculate.on_click(update_data)
-# p = Paragraph(text="{}".format(price_pred * bldg_data['Square Footage']))
 p = Paragraph(text="foobar")
-box = widgetbox([density, nbhd, reno_new, yearbuilt, squarefootage, segment, fixtures, quarter, calculate, p])
+box = widgetbox([density, nbhd, reno_new, yearbuilt, squarefootage, segment, quarter, calculate, p])
 curdoc().add_root(box)
--- myapp/pps_regressor.py
+++ myapp/pps_regressor.py
@@ -4,7 +4,7 @@ import lightgbm as lgb
 import json
 
 DENSITIES = {0:"SINGLE FAM", 1: "PLEX", 2: "CONDO", 3: "HRISE CONDO"}
-SEGMENTS = {0: 'C', 1: 'B', 2: 'A', 3:'X'}
+SEGMENTS = {0: 'D', 1: 'C', 2: 'B'}
 
 def prediction(input_dict):
     model = lgb.Booster(model_file='noyear.txt')
@@ -12,7 +12,6 @@ def prediction(input_dict):
     tester = pd.DataFrame(lst)
     print(tester)
     tester['Log-Built'] = np.log(tester['Year Built'])
-    tester['Log-Fixtures'] = np.log(int(tester['Fixtures']))
     tester['Log-SqFt'] = np.log(int(tester['Square Footage']))
     tester['Neighborhood'] = tester['Neighborhood'].astype(float)
     with open('meanpps.json', 'r') as nbhdpps:
@@ -25,8 +24,8 @@ def prediction(input_dict):
     tester['Neighborhood'] = str(tester['Neighborhood'])
     tester['Building-Grade'] = tester['Building-Grade'].map(lambda x: SEGMENTS[x])
     tester['Building-Type-Custom'] = tester['Building-Type-Custom'].map(lambda x: DENSITIES[x])
-    X = pd.get_dummies(tester[['Year', 'Log-Built', 'Neighborhood', 'Log-Fixtures', 
-                               'Building-Type-Custom', 'Quarter', 'Building-Grade', 
-                               'Log-SqFt', 'Log-NbhdPPS']])
+    X = pd.get_dummies(tester[['Year', 'Log-Built', 'Neighborhood',  'Building-Type-Custom', 
+                               'Quarter', 'Building-Grade', 'Log-SqFt',
+                               'Log-NbhdPPS']])
     price_pred = model.predict(X)
     return price_pred
