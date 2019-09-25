1044b8b1c1419041b0c1986befc789fc229dcdf7
Drew <alehe90@gmail.com>
2019-09-25 18:45:00 -0500

    got everything working, neighborhoods as ints

--- myapp/main.py
+++ myapp/main.py
@@ -10,12 +10,13 @@ import pandas as pd
 with open('meanpps.json', 'r') as nbhdpps:
         nbhd_dict = json.load(nbhdpps)
 meanpps = pd.DataFrame(nbhd_dict, index=None)
-hoods = list(meanpps.index)
+hoods = [str(int(float(i))) for i in meanpps.index]
+
 
 def update_data(event):
     '''Create a dictionary of selected values in the widget when the "Get Price" button is pressed'''
     bldg_data = {
-        'Neighborhood': float(nbhd.value),
+        'Neighborhood': str(nbhd.value),
         'Building-Type-Custom': density.active,
         'Year Built': int(yearbuilt.value_input),
         'Building-Grade': segment.active,
@@ -29,17 +30,16 @@ def update_data(event):
         bldg_data['Year Built'] = int(yearbuilt.value_input)
     price_pred = pps_regressor.prediction(bldg_data)
     p.text = '$' + str(price_pred[0] * bldg_data['Square Footage'])
-#     p.text = str(tester)
 
 # Create some widgets
-nbhd = Select(title="Neighborhood:", options=list(hoods))
+nbhd = Select(title="Neighborhood:", options=hoods)
 density = RadioButtonGroup(labels=['Single-Family', 'Multi-Family'])
 reno_new = RadioButtonGroup(labels=["Renovation", "New Build"])
 #If renovation, select year built of the property:
 yearbuilt = TextInput(placeholder="4-digit #", title="Year of the original structure (if renovation):")
 squarefootage = Slider(start=500, end=3500, value=500, step=10, title="Square Footage")
 segment = RadioButtonGroup(labels=["Affordable", "Mid-Range", "High-End"])
-quarter = RadioGroup(labels = ['First', 'Second', 'Third', 'Fourth'])
+quarter = RadioGroup(labels = ['First Quarter Sale', 'Second Quarter Sale', 'Third Quarter Sale', 'Fourth Quarter Sale'])
 fixtures = Slider(start=3, end=20, value=3, step=1, title="Water Fixtures")
 calculate = Button(label="Get Price", button_type="success")
 calculate.on_click(update_data)
--- myapp/pps_regressor.py
+++ myapp/pps_regressor.py
@@ -12,37 +12,32 @@ def prediction(input_dict):
     '''Take in the bldg_data dictionary as a json, transform the features and give a price prediction'''
     model = lgb.Booster(model_file='pps-model.txt')
     lst = [(input_dict)]
-#     print(lst)
     tester = pd.DataFrame(lst)
     tester['Log-Built'] = np.log(tester['Year Built'])
     tester['Log-SqFt'] = np.log(int(tester['Square Footage']))
-    tester['Neighborhood'] = tester['Neighborhood'].astype(float)
+    tester['Neighborhood'] = tester['Neighborhood'].astype(str)
     with open('meanpps.json', 'r') as nbhdpps:
         nbhd_dict = json.load(nbhdpps)
     meanpps = pd.DataFrame(nbhd_dict, index=None)
-    meanpps.index=meanpps.index.astype(float)
-    nbhds = list(meanpps.index)
-    tester=pd.merge(how = 'left', left = tester, right = meanpps,left_on="Neighborhood", right_index=True)
+    meanpps.index=meanpps.index.astype(str)
+    nbhds = [str(int(float(i))) for i in meanpps.index]
+    tester=pd.merge(how = 'left', left = tester, right = meanpps, left_on="Neighborhood", right_index=True)
     tester['Log-NbhdPPS'] = np.log(tester['NeighborhoodPPS'])
     tester['Neighborhood'] = str(tester['Neighborhood'])
     tester['Building-Grade'] = tester['Building-Grade'].map(lambda x: SEGMENTS[x])
     tester['Building-Type-Custom'] = tester['Building-Type-Custom'].map(lambda x: DENSITIES[x])
     tester['Quarter'] = tester['Quarter'].map(lambda x: QUARTERS[x])
     tester['Year']=2019
-    print(tester[:12])
-    ML = tester[['Year', 'Log-Built', 'Neighborhood',  'Building-Type-Custom', 
+    ML = tester[['Year', 'Log-Built', 'Neighborhood', 'Building-Type-Custom', 
                  'Quarter', 'Building-Grade', 'Log-SqFt', 'Log-NbhdPPS']]
-#     print(ML.dtypes)
-    
+    dummies = pd.get_dummies(ML)
     with open(f'ml_vars.json', 'r') as file:
         ml_vars = json.load(file)
     data = pd.DataFrame(columns=ml_vars)
     data['Year']=data['Year'].astype(int)
     data[['Log-SqFt', 'Log-NbhdPPS', 'Log-Built']] = data[['Log-SqFt', 'Log-NbhdPPS', 'Log-Built']].astype(float)
-    testdummy = pd.get_dummies(ML)
-    X=pd.concat([data,testdummy], axis=0, sort=True)
+    X=pd.concat([data,dummies], axis=0, sort=True)
     X.fillna(value=0, inplace=True)
     X = X[ml_vars]
-#     print(X.columns)
     price_pred = np.abs(model.predict(X))
     return price_pred
