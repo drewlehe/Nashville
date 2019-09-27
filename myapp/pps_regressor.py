39afa7fb6d43359c7d7b8e5aeeb8085d8f264d9d
Upom Malik <upom.malik@gmail.com>
2019-09-27 01:24:51 -0400

    fixed spacing

--- myapp/pps_regressor.py
+++ myapp/pps_regressor.py
@@ -1,10 +1,10 @@
 '''Contains the function to predict the price of the house, given the selected values in the widget.'''
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import lightgbm as lgb
 import json
 
-DENSITIES = {0:"SINGLE FAM", 1: "PLEX"}
+DENSITIES = {0: "SINGLE FAM", 1: "PLEX"}
 SEGMENTS = {0: 'D', 1: 'C', 2: 'B'}
 QUARTERS = {0: '1', 1: '2', 2: '3', 3: '4'}
 
@@ -29,7 +29,7 @@ def prediction(input_dict):
     tester['Building-Type-Custom'] = tester['Building-Type-Custom'].map(lambda x: DENSITIES[x])
     tester['Quarter'] = tester['Quarter'].map(lambda x: QUARTERS[x])
     tester['Year']=2019
-    ML = tester[['Year', 'Log-Built', 'Neighborhood', 'Building-Type-Custom', 
+    ML = tester[['Year', 'Log-Built', 'Neighborhood', 'Building-Type-Custom',
                  'Quarter', 'Building-Grade', 'Log-SqFt', 'Log-NbhdPPS']]
     dummies = pd.get_dummies(ML)
     #Grab column names/order for the dummy variables
